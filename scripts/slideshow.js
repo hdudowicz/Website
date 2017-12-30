e467ca4a8d890c9974ba0b83f8dbc9b839b0d382
hdudowicz <hdudowicz@gmail.com>
2017-12-30 15:36:53 +0000

    Gallery Added

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -2,7 +2,7 @@ var i = 0;
 var images = [];	
 var time = 2000;	
 	 
-images[0] = "images/image1.jpg";
+images[0] = "images/image6.jpg";
 images[1] = "images/image2.jpg";
 images[2] = "images/image3.jpg";
 images[3] = "images/image4.jpg";
